ad4674ec62520cc11ab5a9a1af1c400c6b024f54
LY <daniel1946@163.com>
2017-03-04 00:34:40 +0800

    inherited

--- src/com/ly/annotation/InheritTableSon.java
+++ src/com/ly/annotation/InheritTableSon.java
@@ -2,6 +2,7 @@ package com.ly.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -9,6 +10,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Inherited
 @interface Inheritable{
 	
 }
