9d11c1f8d3449c09053f6e7ed0ac96787a85a636
LY <daniel1946@163.com>
2017-03-04 00:32:43 +0800

    Inherit sample

---
+++ src/com/ly/annotation/InheritTableSon.java
@@ -0,0 +1,32 @@
+package com.ly.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@interface Inheritable{
+	
+}
+
+@Inheritable
+class InheritableFather{
+	public InheritableFather() {
+		System.out.println("InheritableFather:" + InheritableFather.class.isAnnotationPresent(Inheritable.class));
+	}
+}
+public class InheritTableSon extends InheritableFather{
+
+	public InheritTableSon() {
+		System.out.println("InheritableSon:" + InheritTableSon.class.isAnnotationPresent(Inheritable.class));
+	}
+	public static void main(String[] args) {
+
+		InheritTableSon son = new InheritTableSon();
+	}
+
+}
