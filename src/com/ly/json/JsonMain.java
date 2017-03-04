05bcdb185a8080b48ca88241909a0d42f970f954
Liuyan <zhuoying.ly@alibaba-inc.com>
2017-03-04 18:28:46 +0800

    add toObject test

--- src/com/ly/json/JsonMain.java
+++ src/com/ly/json/JsonMain.java
@@ -13,6 +13,7 @@ public class JsonMain {
 	public static void main(String[] args) {
 		
 		testToString();
+		testToObject();
 	}
 	
 	private static void testToString() {
@@ -49,7 +50,21 @@ public class JsonMain {
 		student1.setTeachers(teachers);
 		student1.setCourses(courses);
 		
-		System.out.println(Json.toString(student1));
+//		List<Student> students = new ArrayList<>();
+//		students.add(student1);
+//		
+//		chineseTeacher.setStudents(students);
+		long t1 = System.currentTimeMillis();
+		String jsonString = Json.toString(student1);
+		System.out.println("toString time " + (System.currentTimeMillis() - t1));
+		System.out.println(jsonString);
+	}
+	
+	private static void testToObject() {
+		String jsonString = "{\"teachers\":[{\"course\":{\"name\":\"Chinese\",\"score\":0},\"name\":\"LY\",\"sex\":0,\"age\":50},{\"course\":{\"name\":\"Chinese1\",\"score\":0},\"name\":\"LY1\",\"sex\":0,\"age\":50}],\"courses\":[{\"name\":\"Chinese\",\"score\":0},{\"name\":\"Chinese1\",\"score\":0}],\"name\":\"YL\",\"sex\":0,\"age\":10}";
+		long t1 = System.currentTimeMillis();
+		Student student = Json.toObject(jsonString, Student.class);
+		System.out.println("toObject time " + (System.currentTimeMillis() - t1));
 	}
 
 }
